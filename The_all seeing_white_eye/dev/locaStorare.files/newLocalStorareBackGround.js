8c2bf76b7c17fd948672825049472ee838095c6c
J andres <133991551+Mirror-Scribbles@users.noreply.github.com>
2024-05-24 11:25:29 -0500

    update

---
+++ The_all seeing_white_eye/dev/locaStorare.files/newLocalStorareBackGround.js
@@ -0,0 +1,15 @@
+const $addBackGround = document.querySelector('.add');
+const $clearBackGround = document.querySelector('.clear');
+
+const d = document;
+
+d.addEventListener('click', (e) =>{
+    if (e.target == $addBackGround){
+        const color = 
+        console.log('you have click added a background')
+    }else if (e.target == $clearBackGround){
+        console.log('you have  removed the background')
+    }else{
+        return
+    }
+});
